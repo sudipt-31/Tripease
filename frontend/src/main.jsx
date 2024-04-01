45a552ff7e19e524d824089117d16cf74abf1209
Sudipta Shaw <sudipthere@gmail.com>
2024-04-01 17:55:40 +0530

    in 1st branch

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,14 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
