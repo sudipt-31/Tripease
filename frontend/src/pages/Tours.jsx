93a7e043ab84be8b155837f1812ff8c74ad12cff
Rishav <rishavjaiswal864@gmail.com>
2024-04-01 14:51:54 +0530

    Common Section done

---
+++ frontend/src/pages/Tours.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CommonSection from '../shared/CommonSection'
+const Tours = () => {
+  return (
+    <>
+    <CommonSection title={"All Tours"} />
+    </>
+  )
+}
+
+export default Tours
